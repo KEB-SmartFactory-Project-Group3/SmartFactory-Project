e4dd0ad69e074392ec173f279f7894dc018a299a
leedabin2 <12193152@inha.edu>
2023-07-27 13:02:33 +0900

    loginpage v0.0

---
+++ login-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Login from './Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SmartFactory Monitoring</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
+
