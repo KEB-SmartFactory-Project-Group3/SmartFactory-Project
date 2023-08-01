3f70f49966df298851b2c23e0d88d4f1b12b2f37
leedabin2 <12193152@inha.edu>
2023-08-01 20:00:18 +0900

    login-page OperationTime v0.0

--- login-app/src/Components/SignIn/LandingPage.jsx
+++ login-app/src/Components/SignIn/LandingPage.jsx
@@ -1,13 +1,38 @@
 import { useParams } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
+import axios  from 'axios';
 
 function LandingPage() {
 
   const {id} = useParams()
+  const {operationtime,setOperationtime} = useState(null)
+
+  function callTimeApi() {
+    console.log("called")
+    axios.get("http://172.20.10.3:8080/api/time/operationtime")
+          .then (
+            (response) => successfulResponse(response)
+          )
+          .catch ( (error) => errorResponse(error))
+          .finally ( () => console.log("cleanup"))
+    
+  }
+  function successfulResponse(response) {
+    console.log(response)
+    setOperationtime(response.data)
+  }
+
+  function errorResponse(error) {
+    console.log(error)
+  }
 
   return (
     <div className="landing">
       <h1>Welcome {id}</h1>
+      <div>
+        <button className="btn-time" onClick={callTimeApi}>Time API</button>
+      </div>
+      <div className="operation-info">{operationtime}</div>
     
     </div>
   )
