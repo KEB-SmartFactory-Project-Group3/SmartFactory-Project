09f0e935094446f1f45b472e882eca41172fe2d1
leedabin2 <12193152@inha.edu>
2023-08-02 19:08:14 +0900

    error-page v0.1

--- login-app/src/Components/SignIn/ErrorPage.jsx
+++ login-app/src/Components/SignIn/ErrorPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ErrorIcon from '@mui/icons-material/Error';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -24,13 +23,14 @@ function ErrorPage() {
       }}
     >
       <div className="Errorpage">
-        <ErrorIcon />
-        <h1>Not found</h1>
-        <h3>주소가 잘못되었습니다. 다시 확인해주세요.</h3>
-        <Stack spacing={2} direction="row">
-          <Button variant="outlined"onClick= {() => navigate(-1)}>뒤로가기</Button>
-          <Button variant="outlined" onClick={redirectLogin}>로그인 화면으로 이동</Button>
+        <div>
+        <h1 style={{ textAlign: 'center', marginBottom: '1rem' }} >Not found</h1>
+        <h3 style={{ textAlign: 'center', marginBottom: '2rem' }} >주소가 잘못되었습니다. 다시 확인해주세요.</h3>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Button variant="outlined" sx={{ width: '120px' }} onClick= {() => navigate(-1)}>뒤로가기</Button>
+          <Button variant="outlined" sx={{ width: '180px' }} onClick={redirectLogin}>로그인 화면으로 이동</Button>
         </Stack>
+        </div>
       </div>
     </Box>
   )
