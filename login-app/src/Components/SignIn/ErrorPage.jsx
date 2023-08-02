fdff77d1c06a9949f2ce1e7a07e86794cf15e70f
leedabin2 <12193152@inha.edu>
2023-08-02 18:31:40 +0900

    error-page v0.1

--- login-app/src/Components/SignIn/ErrorPage.jsx
+++ login-app/src/Components/SignIn/ErrorPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ErrorIcon from '@mui/icons-material/Error';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
+
+  const navigate = useNavigate();
+
+  function redirectLogin() {
+     navigate("/")
+  }
 
   return (
     <Box
@@ -16,8 +25,12 @@ function ErrorPage() {
     >
       <div className="Errorpage">
         <ErrorIcon />
-        <h1>페이지가 없어요</h1>
+        <h1>Not found</h1>
         <h3>주소가 잘못되었습니다. 다시 확인해주세요.</h3>
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined"onClick= {() => navigate(-1)}>뒤로가기</Button>
+          <Button variant="outlined" onClick={redirectLogin}>로그인 화면으로 이동</Button>
+        </Stack>
       </div>
     </Box>
   )
