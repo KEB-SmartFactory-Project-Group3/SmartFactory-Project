e3105a5c7ae836432ea6f128c539a86b02c883e7
leedabin2 <12193152@inha.edu>
2023-08-02 18:02:45 +0900

    error-page v0.0

--- login-app/src/Components/SignIn/ErrorPage.jsx
+++ login-app/src/Components/SignIn/ErrorPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import ErrorIcon from '@mui/icons-material/Error';
+import Box from '@mui/material/Box';
 
 function ErrorPage() {
 
   return (
-    <div className="Errorpage">
-      <h1>error!</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '50vh',
+      }}
+    >
+      <div className="Errorpage">
+        <ErrorIcon />
+        <h1>페이지가 없어요</h1>
+        <h3>주소가 잘못되었습니다. 다시 확인해주세요.</h3>
+      </div>
+    </Box>
   )
 }
 
