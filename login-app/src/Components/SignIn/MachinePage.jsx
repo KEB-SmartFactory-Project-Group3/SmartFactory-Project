d8bdbb6b1de051bc9ecb9b0ec7d602509ecc4d25
leedabin2 <12193152@inha.edu>
2023-08-05 14:10:37 +0900

    machine-page grid ui

--- login-app/src/Components/SignIn/MachinePage.jsx
+++ login-app/src/Components/SignIn/MachinePage.jsx
@@ -6,11 +6,6 @@ import useMachineCount from '../hooks/useMachineCount';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import FormDialog from './FormDialog';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
 import Paper from '@mui/material/Paper';
 
 
@@ -39,72 +34,189 @@ function MachinePage() {
 
   return (
     <div className="MachinePage">
-      <Grid sx={{ flexGrow: 1 }} container spacing={5}>
-        <Grid item xs={12}>
-          <Grid container justifyContent="center" spacing={12}>
-            {[0, 1, 2].map((value) => (
-              <Grid key={value} item>
-                <Paper
-                  sx={{
-                    height: 300,
-                    width: 300,
-                    backgroundColor: (theme) =>
+       <Grid container spacing={3}>
+        <Grid item xs={4} md = {4}>
+          <Box sx={{p:6.5}}>
+            <Paper 
+                sx={{
+                  height: 550,
+                  width: 350,
+                  backgroundColor: (theme) =>
                       theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                  }}
-                />
-              </Grid>
-            ))}
-         </Grid>
-       </Grid>
-       <Grid item sx={12}>
-       <Box sx={{ '& button': { m: 0.5 } }}>
-          <h1>기계 장치</h1>
-          <div className="targetAchievement-info">도달량 : {targetAchievement}</div>
-          <div className="targetProduction-info">목표 생산량 : {targetProduction}</div>
-          <div className="operation-info">총 가동 시간 : {operationTime}</div>
-          <div className="production-info">생산량 : {production}</div> 
-          <p>가동중지: {formatTime(elapsedTime)}</p>
-        <div>
-          {isRunning ? (
-            <Button
-              variant="contained"
-              style={{ backgroundColor: '#5C6AC4', color: 'white' }}
-              sx={{ width: '20px' }}
-              size="small"
-              onClick={handleStop}
+                }}
             >
-              Stop
-            </Button>
-          ) : (
-            <Button
-              variant="contained"
-              style={{ backgroundColor: '#5C6AC4', color: 'white' }}
-              sx={{ width: '20px' }}
-              size="small"
-              onClick={() => {
-                handleStart();
-                handleOpenForm();
-              }}
-            >
-              Start
-            </Button>
-          )}
-          <FormDialog open={formOpen} handleClose={handleCloseForm} />
-          <Button
-            variant="contained"
-            style={{ backgroundColor: '#5C6AC4', color: 'white' }}
-            sx={{ width: '20px' , marginRight: '10px'}}
-            size="small"
-            onClick={resetTimer}
-          >
-            Reset
-          </Button>
-          </div>
+              <item>
+                <Box sx={{ '& button': { m: 0.5 } }}>
+                  <h1>A</h1>
+                  <div className="targetAchievement-info">도달량 : {targetAchievement}</div>
+                  <div className="targetProduction-info">목표 생산량 : {targetProduction}</div>
+                  <div className="operation-info">총 가동 시간 : {operationTime}</div>
+                  <div className="production-info">생산량 : {production}</div> 
+                  <p>가동중지: {formatTime(elapsedTime)}</p>
+                <div>
+                {isRunning ? (
+                  <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                    sx={{ width: '20px' }}
+                    size="small"
+                    onClick={handleStop}
+                  >
+                    Stop
+                  </Button>
+                  ) : (
+                  <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                    sx={{ width: '20px' }}
+                    size="small"
+                    onClick={() => {
+                      handleStart();
+                      handleOpenForm();
+                    }}
+                  >
+                    Start
+                  </Button>
+                )}
+                <FormDialog open={formOpen} handleClose={handleCloseForm} />
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                  sx={{ width: '20px' , marginRight: '10px'}}
+                  size="small"
+                  onClick={resetTimer}
+                >
+                  Reset
+                </Button>
+                </div>
+              </Box>
+              </item>
+          </Paper>
           </Box>
+        </Grid>
+          <Grid item xs={4} md ={4}>
+            <Box sx={{p:6.5}}>
+              <Paper
+                  sx = {{
+                    height: 550,
+                    width: 350,
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                  }}
+                >
+              <item>
+              <Box sx={{ '& button': { m: 0.5 } }}>
+                <h1>B</h1>
+                <div className="targetAchievement-info">도달량 : {targetAchievement}</div>
+                <div className="targetProduction-info">목표 생산량 : {targetProduction}</div>
+                <div className="operation-info">총 가동 시간 : {operationTime}</div>
+                <div className="production-info">생산량 : {production}</div> 
+                <p>가동중지: {formatTime(elapsedTime)}</p>
+               <div>
+              {isRunning ? (
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                  sx={{ width: '20px' }}
+                  size="small"
+                  onClick={handleStop}
+                >
+                  Stop
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                  sx={{ width: '20px' }}
+                  size="small"
+                  onClick={() => {
+                    handleStart();
+                    handleOpenForm();
+                  }}
+                >
+                  Start
+                </Button>
+              )}
+              <FormDialog open={formOpen} handleClose={handleCloseForm} />
+              <Button
+                variant="contained"
+                style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                sx={{ width: '20px' , marginRight: '10px'}}
+                size="small"
+                onClick={resetTimer}
+              >
+                Reset
+              </Button>
+              </div>
+              </Box>
+              </item>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <Box sx={{p:6.5}}>
+              <Paper
+                  sx = {{
+                    height: 550,
+                    width: 350,
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                  }}
+                >
+              <item>
+              <Box sx={{ '& button': { m: 0.5 } }}>
+                <h1>C</h1>
+                <div className="targetAchievement-info">도달량 : {targetAchievement}</div>
+                <div className="targetProduction-info">목표 생산량 : {targetProduction}</div>
+                <div className="operation-info">총 가동 시간 : {operationTime}</div>
+                <div className="production-info">생산량 : {production}</div> 
+                <p>가동중지: {formatTime(elapsedTime)}</p>
+               <div>
+              {isRunning ? (
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                  sx={{ width: '20px' }}
+                  size="small"
+                  onClick={handleStop}
+                >
+                  Stop
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                  sx={{ width: '20px' }}
+                  size="small"
+                  onClick={() => {
+                    handleStart();
+                    handleOpenForm();
+                  }}
+                >
+                  Start
+                </Button>
+              )}
+              <FormDialog open={formOpen} handleClose={handleCloseForm} />
+              <Button
+                variant="contained"
+                style={{ backgroundColor: '#5C6AC4', color: 'white' }}
+                sx={{ width: '20px' , marginRight: '10px'}}
+                size="small"
+                onClick={resetTimer}
+              >
+                Reset
+              </Button>
+              </div>
+              </Box>
+              </item>
+              </Paper>
+            </Box>
+          </Grid>
 
-       </Grid>
-      </Grid> 
-    </div>
+          
+       
+      </Grid>
+  </div>
   );
 }
 
