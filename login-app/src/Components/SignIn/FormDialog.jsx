d7160aa1fb278d3e7bd6e143d5b6d5e4cff1e45f
leedabin2 <12193152@inha.edu>
2023-08-10 17:06:40 +0900

    shutdown form v0.0

--- login-app/src/Components/SignIn/FormDialog.jsx
+++ login-app/src/Components/SignIn/FormDialog.jsx
@@ -5,25 +5,25 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function FormDialog({ open, handleClose }) {
-  const [checkedItems, setCheckedItems] = useState([])
+  const [selectedReason, setSelectedReason] = useState('')
 
-  const handleCheckboxChange = (value) => {
-    if (checkedItems.includes(value)) {
-      setCheckedItems(checkedItems.filter(item => item !== value));
-    } else {
-      setCheckedItems([...checkedItems, value]);
-    }
-}
-  const handleReasonSubmit = () => {
-    // 선택된 항목들을 가공하여 처리하는 로직 추가
-    console.log('Selected items:', checkedItems);
-    handleClose();
+  const handleReasonChange = (event) => {
+    setSelectedReason(event.target.value);
   }
 
-  
+  const handleReasonSubmit = () => {
+    // 선택된 항목을 처리하는 로직 추가
+    if (selectedReason) {
+      console.log('Selected reason:', selectedReason)
+    }
+    handleClose()
+  }
+
 
   return (
     <Dialog open={open} onClose={handleClose}>
@@ -32,31 +32,33 @@ export default function FormDialog({ open, handleClose }) {
         <DialogContentText>
           가동 중지하는 이유를 선택해주세요
         </DialogContentText>
+        <RadioGroup value={selectedReason} onChange={handleReasonChange}>
         <FormControlLabel
-          control={<Checkbox />}
-          label="휴식"
-          onChange={() => handleCheckboxChange('휴식')}
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="교대"
-          onChange={() => handleCheckboxChange('교대')}
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="고장"
-          onChange={() => handleCheckboxChange('고장')}
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="점검"
-          onChange={() => handleCheckboxChange('점검')}
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="사고"
-          onChange={() => handleCheckboxChange('사고')}
-        />
+            value="휴식"
+            control={<Radio />}
+            label="휴식"
+          />
+          <FormControlLabel
+            value="교대"
+            control={<Radio />}
+            label="교대"
+          />
+          <FormControlLabel
+            value="고장"
+            control={<Radio />}
+            label="고장"
+          />
+          <FormControlLabel
+            value="점검"
+            control={<Radio />}
+            label="점검"
+          />
+          <FormControlLabel
+            value="사고"
+            control={<Radio />}
+            label="사고"
+          />
+        </RadioGroup>
       </DialogContent>
       <DialogActions>
         <button onClick={handleClose}>취소</button>
