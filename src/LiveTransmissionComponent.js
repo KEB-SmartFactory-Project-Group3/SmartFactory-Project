6f0b769d6dafd583c083ebf79190a252699c69bf
kaxadlec <kaxadlec@gmail.com>
2023-08-15 01:07:42 +0900

    chore: Remove image capture feature

--- src/LiveTransmissionComponent.js
+++ src/LiveTransmissionComponent.js
@@ -7,27 +7,12 @@ const LiveTransmissionComponent = () => {
     const [liveImage, setLiveImage] = useState('');
 
     const fetchLiveImage = async () => {
-    try {
+        try {
         const response = await fetch('http://127.0.0.1:5000/get-live-transmission');
         const data = await response.json();
         setLiveImage(`data:image/jpeg;base64,${data.image}`);
-    } catch (error) {
+        } catch (error) {
         console.error('Error fetching live image:', error);
-    }
-    };
-
-    const captureImage = async () => {
-        if (liveImage) {
-            try {
-            // 서버로 가공되지 않은 데이터 URL을 전송합니다.
-            const response = await axios.post("http://localhost:5000/capture-image", { liveImage });
-            
-            // 응답이 올바르게 반환되는지 확인하기 위해 결과를 다룹니다.
-            console.log(response.data);
-
-            } catch (error) {
-            console.error("Error capturing image:", error);
-            }
         }
     };
 
@@ -46,9 +31,6 @@ const LiveTransmissionComponent = () => {
             ) : (
             <p>Loading live transmission...</p>
             )}
-            <button className="capture-button" onClick={captureImage}>
-            Capture Image
-            </button>
         </div>
         );
 };
