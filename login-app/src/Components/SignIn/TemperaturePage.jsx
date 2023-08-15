ec16ff4a58e8075d3124e4dc9ce6fd13ad759abc
leedabin2 <12193152@inha.edu>
2023-08-15 22:41:11 +0900

    temperature-page grid v0.0

--- login-app/src/Components/SignIn/TemperaturePage.jsx
+++ login-app/src/Components/SignIn/TemperaturePage.jsx
@@ -1,10 +1,74 @@
 import React from 'react';
+import { StyledBackground, StyledFactor } from '../stylescomp/BackgroundStyle';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { TempGridStyled,TempItemStyled, TempHumItemStyled } from '../stylescomp/TemperatureStyle';
+import { CSSTransition } from 'react-transition-group';
+import { FadeBox } from '../stylescomp/FadeinStyle';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function TemperaturePage() {
+
+
   return (
-    <div className="TemperaturePage">
-      <h1>온도</h1>
-    </div>
+    <StyledBackground>
+      <StyledFactor>
+      <CSSTransition in={true} timeout={1000} classNames="fade" appear>
+      <FadeBox sx={{flexGrow:1}}>
+      <Grid container spacing={2} sx={{ background: 'transparent', border: 'none' , boxShadow: 'none',}}>
+      <TempGridStyled item xs={12} sm={4} md={4}>
+            <TempItemStyled>
+              현재 온도
+            </TempItemStyled>
+        </TempGridStyled>
+
+        <TempGridStyled item xs={12} sm={4} md={4}>
+            <TempItemStyled>
+              현재 습도
+            </TempItemStyled>
+        </TempGridStyled>
+
+        <TempGridStyled item xs={12} sm={4} md={4}>
+        <Grid container direction="column">
+          <TempGridStyled item xs={12}>
+            <TempHumItemStyled>
+              적정 온도
+            </TempHumItemStyled>
+          </TempGridStyled>
+          <TempGridStyled item xs={12}>
+            <TempHumItemStyled>
+              적정 습도
+            </TempHumItemStyled>
+          </TempGridStyled>
+        </Grid>
+      </TempGridStyled>
+
+      <TempGridStyled item xs={12} sm={12} md={12}>
+          <TempItemStyled>
+            실시간 온도 그래프
+          </TempItemStyled>
+      </TempGridStyled>
+
+      <TempGridStyled item xs={12} sm={12} md={12}>
+          <TempItemStyled>
+            일별 온습도 평균 최대 최솟값
+          </TempItemStyled>
+      </TempGridStyled>
+
+      </Grid>
+      </FadeBox>
+      </CSSTransition>
+      </StyledFactor>
+    </StyledBackground>
+ 
   )
 }
 
---
+++ login-app/src/Components/stylescomp/TemperatureStyle.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import Grid from '@mui/material/Grid';
+import { TextField } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+
+export const TempGridStyled = styled(Grid)`
+  // 기본 스타일
+  box-sizing: border-box;
+
+`;
+
+export const TempItemStyled = styled.div`
+  position: relative;
+  margin: 5px 0; // 각 그리드 마진 조정
+  border: 2px solid ${props => props.borderColor || 'white'};
+  box-shadow: none; 
+  margin-top: 25px; 
+  border-radius: 5px; 
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(8px);
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  height: 22vh;
+  text-align: center; 
+  cursor: pointer;
+  overflow: hidden;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+
+// 온슫도 item
+export const TempHumItemStyled = styled.div`
+  position: relative;
+  margin: 5px 0; // 각 그리드 마진 조정
+  border: 2px solid ${props => props.borderColor || 'white'};
+  box-shadow: none; 
+  margin-top: 25px; 
+  border-radius: 5px; 
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(8px);
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  height: 11vh;
+  text-align: center; 
+  cursor: pointer;
+  overflow: hidden;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
