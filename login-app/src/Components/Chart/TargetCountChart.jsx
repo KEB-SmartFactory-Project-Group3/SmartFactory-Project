0711a0f87787e25a0f0f9ca9aa44436011902b53
leedabin2 <12193152@inha.edu>
2023-08-15 17:38:42 +0900

    nowRate Alert v0.0

--- login-app/src/Components/Chart/TargetCountChart.jsx
+++ login-app/src/Components/Chart/TargetCountChart.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import { PieChart, Pie, Cell } from 'recharts';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 import useMachineRate from '../hooks/useMachineRate';
 
 function TargetDonutChart({ nowRate }) {
     // const {nowRate} = useMachineRate()
+    const [open, setOpen] = useState(false)
     const percentage = parseFloat((nowRate * 100).toFixed(1));
+
+    useEffect(() => {
+        if (nowRate >= 0.9 && !open) {
+            setOpen(true);
+        }
+    }, [nowRate]);
+
     const data = [
         { name: 'Achieved', value: percentage },
         { name: 'Remaining', value: 100 - percentage }
@@ -39,6 +49,13 @@ function TargetDonutChart({ nowRate }) {
                              percentage < 70 ? 'green' : 'red'}}>
             {percentage}%
         </div>
+        
+        {/* // nowRate가 90% 이상일 때마다 알림 표시 */}
+        <Snackbar open={open} autoHideDuration={6000} onClose={() => setOpen(false)}>
+            <Alert onClose={() => setOpen(false)} severity="warning" variant="filled">
+                도달률이 90% 이상입니다!
+            </Alert>
+        </Snackbar>
        
         </div>
     );
