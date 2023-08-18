f92fb9422a2bd6ebc88c022fb4a34fa41120eb0c
leedabin2 <12193152@inha.edu>
2023-08-18 19:41:53 +0900

    operation management api

--- login-app/src/Components/api/ApiService.js
+++ login-app/src/Components/api/ApiService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
+export const apiServer = axios.create({
+    baseURL: 'http://192.168.43.183:8080',
+    withCredentials: true, //쿠키 자동 포함
+  })
+
+export const apiClient = axios.create({
   baseURL: 'http://192.168.43.183:8080',
   withCredentials: true, //쿠키 자동 포함
 });
@@ -29,7 +34,7 @@ export const sendStartToBackend = async (StartData) => {
   }
 };
 
-// reset 백엔드 api 전송
+//  reset 백엔드 api 전송
 export const sendResetToBackend = async (ResetData) => {
   const token = document.cookie.split('=')[1]; // 쿠키에서 토큰 가져오기
     const config = {
@@ -38,9 +43,10 @@ export const sendResetToBackend = async (ResetData) => {
       },
     }
 try {
-    const response = await axios.post('/click/button', ResetData, config) 
+    const response = await apiClient.post('/click/button', ResetData, config) 
     if (response.status === 200) {
       console.log("resetState posted successfully", response.data)
+      return response.data
     } else {
       console.log("resetState Error while posting")
     }
@@ -50,7 +56,7 @@ try {
 
 }
 
-
+// 백엔드 api 호출 
 export const retrieveData = async (dataKey) => {
   try {
     const token = document.cookie.split('=')[1]; // 쿠키에서 토큰 가져오기
@@ -65,14 +71,33 @@ export const retrieveData = async (dataKey) => {
     if (dataKey === 'factoryTemperature' || dataKey === 'times' || dataKey === 'factoryHumidity') {
       response = await apiClient.get("/api/display/factoryinfo", config);
       return response.data[dataKey];  // dataKey에 대한 값 직접 반환
-    } else {
-      response = await apiClient.get("/api/display/machineinfo", config);
-      if (dataKey === 'count') {
-        return response.data.count
-      } else if (dataKey ==='nowRate') {
-        return response.data.nowRate
-      }
+    } 
+    // else if (dataKey === 'maxTemperature' || dataKey === 'minTemperature' || dataKey === 'avgTemperature' || dataKey === 'maxHumidity' || dataKey === 'minHumidity' || dataKey === 'avgHumidity') {
+    //   response = await apiClient.get("/api/display/statistics", config);
+    //   return response.data[dataKey];
+    // } 
+    
+      else if (dataKey === 'operationStopList') {
+      response = await apiClient.get("/operationstop/totallist", config);
+      const modifiedData = response.data.map(item => ({
+          stopTime: item.operationStopTime,
+          opTime: item.operationTime,
+          user: item.userName,
+          stopReason: item.reason, 
+          rate: item.nowRate
+      }));
+      return modifiedData;
     }
+      else {
+        response = await apiClient.get("/api/display/machineinfo", config);
+        if (dataKey === 'count') {
+          return response.data.count
+        } else if (dataKey ==='nowRate') {
+          return response.data.nowRate
+        } else if (dataKey === 'defectiveCount') {
+          return response.data.defectiveCount
+        }
+      }
       return null 
     } catch (error) {
       console.log("API 호출 오류:", error)
