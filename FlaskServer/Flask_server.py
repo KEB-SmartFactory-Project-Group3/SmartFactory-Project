158526195c53e3a6dd656feb4de78973e36561ef
kaxadlec <kaxadlec@gmail.com>
2023-08-18 17:15:26 +0900

    feat: Arduino count-based data value setting complete

--- FlaskServer/Flask_server.py
+++ FlaskServer/Flask_server.py
@@ -33,6 +33,7 @@ count = 0
 defective_count = 0
 count_from_arduino = 0
 timestamp_from_arduino = None
+pre_count = 0
 
 # 물건의 일련번호 생성기
 serial_counter = 1  # 시리얼 넘버의 시작 숫자를 설정
@@ -69,8 +70,7 @@ def process_and_encode_image(results):
     return encoded_img
 
 
-def generate_serial_number(most_common_name):
-    global serial_counter
+def generate_serial_number(most_common_name, count):
 
     # most_common_name에 따라 접두사 선택
     if most_common_name == "Defective":
@@ -78,8 +78,7 @@ def generate_serial_number(most_common_name):
     else:
         prefix = 'AN'
 
-    serial_number = f"{prefix}{serial_counter:04d}"  # 'AD' or 'AN' 접두사와 4자리 숫자를 결합
-    serial_counter += 1  # 다음 시리얼 넘버를 사용할 때 카운터를 증가
+    serial_number = f"{prefix}{count:04d}"  # 'AD' or 'AN' 접두사와 4자리 숫자를 결합
 
     return serial_number
 
@@ -113,8 +112,7 @@ def receive_data_from_arduino():
 
 def wrapped_get_live_transmission():
     global last_time, latest_detected_object_names, latest_detected_time, first_detection_done, count, defective_count
-    global count_from_arduino, timestamp_from_arduino
-    pre_count = 0
+    global count_from_arduino, timestamp_from_arduino, pre_count
 
     while True:
         # 이미지 가져오기 및 처리
@@ -128,46 +126,44 @@ def wrapped_get_live_transmission():
 
         current_time = time.time()  # 현재 시각
 
-        if not first_detection_done and detected_object_names:
-            first_detection_done = True
+        # 클래스가 검출되지 않고 직전에 검출된 시간이 3초 이내이면 직전에 검출된 클래스 이름을 사용
+        if not detected_object_names and latest_detected_object_names and (current_time - latest_detected_time <= 3):
+            detected_object_names = latest_detected_object_names
+        else:
+            latest_detected_object_names = detected_object_names  # 검출된 클래스 이름을 저장
+            latest_detected_time = current_time  # 검출된 시간을 저장
 
-        if first_detection_done:  # 첫 번째 객체 검출이 완료된 경우에만 다음 작업을 수행
-            # 클래스가 검출되지 않고 직전에 검출된 시간이 3초 이내이면 직전에 검출된 클래스 이름을 사용
-            if not detected_object_names and latest_detected_object_names and (current_time - latest_detected_time <= 3):
-                detected_object_names = latest_detected_object_names
-            else:
-                latest_detected_object_names = detected_object_names  # 검출된 클래스 이름을 저장
-                latest_detected_time = current_time  # 검출된 시간을 저장
+        # 가장 빈번한 클래스 이름을 선택
+        counter = Counter(detected_object_names)
+        most_common = counter.most_common(1)
 
-            if current_time - last_time >= print_interval:
-                if detected_object_names:  # 이 부분을 추가하여, detected_object_names가 빈 리스트가 아닌 경우에만 작업 수행
-                    count += 1  # 카운트 증가
+        if most_common:
+            object_status = most_common[0][0]
+        else:
+            # 빈 리스트의 경우 적절한 처리를 수행
+            object_status = "Normal"
 
-                    # 가장 빈번한 클래스 이름을 선택
-                    counter = Counter(detected_object_names)
-                    most_common = counter.most_common(1)
+        # print(object_status)
 
-                    if most_common:
-                        object_status = most_common[0][0]
-                    else:
-                        # 빈 리스트의 경우 적절한 처리를 수행
-                        object_status = "None"
+        if pre_count != 0 and count_from_arduino == 1:
+            defective_count = 0
 
-                    print(object_status)
-                    if count_from_arduino == pre_count + 1:
-                        # 일련번호 생성
-                        serial_number = generate_serial_number(object_status)
-                        # Defective이면 defective_count를 증가시킴.
-                        if object_status == "Defective":
-                            defective_count += 1
-                        # 데이터를 백엔드 서버로 전송
-                        response = send_to_backend(serial_number, object_status, defective_count, count_from_arduino, timestamp_from_arduino)
-                        print(response)
-                        print("Backend Response:", response.text) # 응답을 콘솔에 출력
+        if count_from_arduino > pre_count:
+            # 일련번호 생성
+            serial_number = generate_serial_number(object_status, count_from_arduino)
+            # Defective이면 defective_count를 증가시킴.
+            if object_status == "Defective":
+                defective_count += 1
+            # 데이터를 백엔드 서버로 전송
+            if object_status == "normal":
+                object_status == "Normal"
+            response = send_to_backend(serial_number, object_status, defective_count, count_from_arduino, timestamp_from_arduino)
+            print(response)
+            print("Backend Response:", response.text) # 응답을 콘솔에 출력
 
-                        pre_count = count_from_arduino
+            pre_count = count_from_arduino
 
-                    last_time = current_time
+        last_time = current_time
 
     # encoded_img = process_and_encode_image(results)
     # return jsonify({'image': encoded_img})
