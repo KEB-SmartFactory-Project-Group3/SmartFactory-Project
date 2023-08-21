4b41e66fef0e383ef8c907d3154549fb4c8fe2a8
leedabin2 <12193152@inha.edu>
2023-08-21 19:45:00 +0900

    chore : app latest version

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from './Components/SignIn/SignIn';
-import { Container } from '@mui/material';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
     <div className="App">
+       <Helmet>
+        <title>smart-factory</title> 
+      </Helmet>
       <SignIn />
     </div>
   );
